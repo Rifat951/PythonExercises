01150d92c8f7438f8abcf3678a6a5f29c5b33cd6
Rifat951 <31892050+Rifat951@users.noreply.github.com>
2021-05-10 15:04:37 +0600

    Create ex7.3.py

---
+++ inpuy_whileloop/ex7.3.py
@@ -0,0 +1,10 @@
+# 7-3. Multiples of Ten: Ask the user for a number, and then report whether the
+# number is a multiple of 10 or not
+
+print("Enter the user count \n")
+table_count = int(input())
+
+if table_count % 10 == 0:
+    print("ok desu")
+else:
+    print("not ok desu")
