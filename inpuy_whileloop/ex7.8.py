5a76ed8e14adfc24eeb3b6e8121a4940c6d5f54d
Rifat951 <31892050+Rifat951@users.noreply.github.com>
2021-05-13 11:11:14 +0600

    Add files via upload

---
+++ inpuy_whileloop/ex7.8.py
@@ -0,0 +1,22 @@
+# 7-8. Deli: Make a list called sandwich_orders and fill it with the names of various sandwiches Then make an empty list called finished_sandwiches Loop
+# through the list of sandwich orders and print a message for each order, such
+# as I made your tuna sandwich. As each sandwich is made, move it to the list
+# of finished sandwiches After all the sandwiches have been made, print a
+# message listing each sandwich that was made
+
+
+
+acitve_loop = True
+
+ingredients = []
+
+while acitve_loop:
+    san_ing = input("Enter Ingredients : ")
+
+    if san_ing == "quit":
+        break
+    else:
+        ingredients.append(san_ing)
+
+for i in ingredients:
+    print("I made {} sandwich".format(i))
